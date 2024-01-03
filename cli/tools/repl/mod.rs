d0837c689d49f843ccd05fd0de790b297caa437e
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2024-01-03 18:16:21 +0100

    add custom deno_ast fork

--- cli/tools/repl/mod.rs
+++ cli/tools/repl/mod.rs
@@ -180,7 +180,7 @@ pub async fn run(flags: Flags, repl_flags: ReplFlags) -> Result<i32, AnyError> {
   // Doing this manually, instead of using `log::info!` because these messages
   // are supposed to go to stdout, not stderr.
   if !cli_options.is_quiet() {
-    println!("Deno {}", crate::version::deno());
+    println!("Deno {} modified (unyt.org)", crate::version::deno());
     println!("exit using ctrl+d, ctrl+c, or close()");
     if repl_flags.is_default_command {
       println!(
